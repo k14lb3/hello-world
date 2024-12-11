4ef3e9e2fb001cc496ef31e30c6257bc38c4c21a
k14lb3 <k14lb3@gmail.com>
2024-12-12 00:17:07 +0800

    🌎 Hello, World!

---
+++ index.js
@@ -0,0 +1,14 @@
+var H = 'H';
+var e = 'e';
+var l = 'l';
+var o = 'o';
+var comma = ',';
+var space = ' ';
+var W = 'W';
+var r = 'r';
+var d = 'd';
+var exclamationMark = '!';
+
+console.log(
+  H + e + l + l + o + comma + space + W + o + r + l + d + exclamationMark,
+);
